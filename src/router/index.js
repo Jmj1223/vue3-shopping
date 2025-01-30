3a9b9f670fcb90f474993a0f29a52c42bab600cb
江明靖 <2698581621@qq.com>
2025-01-30 16:16:46 +0800

    Layout界面

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import Layout from '@/views/Layout/index.vue'
+import Login from '@/views/Login/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/',
+      name: 'layout',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/Layout/index.vue'),
+      children: [
+        {
+          path: 'category',
+          name: 'category',
+          component: () => import('@/views/Category/index.vue')
+        },
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/views/Home/index.vue')
+        }
+      ]
     },
   ],
 })
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+</script>
+
+<template>
+    <LayoutNav />
+    <LayoutHeader />
+    <!-- 二级路由出口 -->
+    <RouterView />
+    <LayoutFooter />
+</template>
