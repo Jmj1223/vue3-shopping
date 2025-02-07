676503cb04a769b1ceace52ee550f7312573d489
江明靖 <2698581621@qq.com>
2025-02-07 16:38:12 +0800

    二级列表—列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -38,6 +38,20 @@ const tabChange = () => {
     reqData.value.page = 1
     getSubCategory()
 }
+
+// 实现无限滚动
+const disabled = ref(false)
+const load = async () => {
+    console.log('滚动到底了!')
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = [...goodList.value, ...res.result.items]
+    // 页面加载完毕 停止监听
+    // 注意 res.result.items.length 而不是 res.result.items.length()
+    if (res.result.items.length === 0) {
+        disabled.value = true
+    }
+}
 </script>
 
 <template>
@@ -58,7 +72,7 @@ const tabChange = () => {
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodItem v-for="good in goodList" :good="good" :key="good.id" />
             </div>
