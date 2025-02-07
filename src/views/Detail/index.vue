f0109ec59b4eca76578a661bd4273be596306f31
江明靖 <2698581621@qq.com>
2025-02-08 00:18:40 +0800

    详情页—热搜区—基础组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { getDetail } from '@/apis/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -125,7 +126,10 @@ onMounted(() => {
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <!-- 24热榜 -->
+                            <DetailHot />
+                            <!-- 专题推荐 -->
+                            <DetailHot />
                         </div>
                     </div>
                 </div>
